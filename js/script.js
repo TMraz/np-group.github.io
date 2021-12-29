4e3c6382c3975959e646b8646018009eafe86b1f
TMraz <87977342+TMraz@users.noreply.github.com>
2021-12-29 10:54:24 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    // === hamburger menu ===
+    $('ul.toggle').click(function(){
+         $(this).toggleClass('active')
+         $('.sidebar').toggleClass('active');
+     });
+
+
+     $('.sidebar .menu-item').click(function(){
+        $('.sidebar').toggleClass('active')
+        $('ul.toggle').toggleClass('active');
+    });
+        
+});
+
+
+
