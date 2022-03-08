05f9d66d7c28a9599405b442d986ce7c972e111e
TMraz <87977342+TMraz@users.noreply.github.com>
2022-03-08 14:34:19 +0100

    Add files via upload

--- js/script_smartmedic.js
+++ js/script_smartmedic.js
@@ -31,9 +31,52 @@ $(document).ready(function(){
         delay: 0
     });
 
-    ScrollReveal().reveal('#about .line', { origin: 'bottom'});
-    ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 300}});
+    ScrollReveal().reveal('.main-title, .post-title, .paragraph, .line:not(.nojs)', {origin: 'bottom'});
 
+    if (window.matchMedia('(min-width:901px) and (max-height: 650px)').matches) {
+        ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 0}});
+    };
+
+    if (window.matchMedia('(min-width:799px) and (max-width:999px) and (min-height: 1101px)').matches) {
+        ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 500}});
+    };
+
+    if (window.matchMedia('(min-width:799px) and (max-width:999px) and (min-height: 1201px)').matches) {
+        ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 700}});
+    };
+
+    if (window.matchMedia('(min-width:401px) and (max-height: 900px)').matches) {
+        ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 0}});
+    };
+
+    if (window.matchMedia('(min-width:599px) and (max-width:799px) and (min-height: 901px)').matches) {
+        ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 500}});
+    };
+
+    if (window.matchMedia('(max-width:400px) and (max-height: 900px)').matches) {
+        ScrollReveal().reveal('#about .main-title, #about .post-title, #about .paragraph, #about .line', {origin: 'bottom', viewOffset: {bottom: 300}});
+    };
+
+    //teardown hide
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 1600) {
+    //         $('#teardown-image').fadeOut('fast')
+    //     } else {
+    //         $('#teardown-image').fadeIn('slow')
+    //     }
+    // });
+
+    // if (window.matchMedia('(min-width:401px) and (min-height: 901px)').matches) {
+    //     $(window).scroll(function () {
+    //         if ($(this).scrollTop() > 800) {
+    //             $('#teardown-image').fadeOut('fast')
+    //         } else {
+    //             $('#teardown-image').fadeIn('slow')
+    //         }
+    //     });
+    // };
+
+    
     
 });
 
